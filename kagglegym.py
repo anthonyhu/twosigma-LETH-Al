27dedd3aa148acc7d36a03da1167742220250e35
Anthony Hu <anth.hu@gmail.com>
2017-01-29 12:48:18 +0000

    Updated kagglegym

--- kagglegym.py
+++ kagglegym.py
@@ -1,4 +1,4 @@
-# This file minics the kagglegym api, including scoring
+# This file minics the kagglegym api, including scoring.
 #
 # You can use it to run experiments locally on the sample training set..
 # To do this follow the following steps.
@@ -39,16 +39,18 @@
 #        rewards.append(reward)
 #        n = n + 1
 #
+#    print(info)
 #    print(n)
 #    print(rewards[0:15])
 #
-# Running 
+# Running
 #    python test_gym.py
 #
 # Should result in:
 #
 #  968
 #  806298
+#  {'public_score': -0.42791846067884648}
 #  906
 # [-0.57244240701870674, -0.69602355761934143, -0.76969605094330085,
 # -0.88292556152797097, -0.77409471948462827, -0.62031952966389658,
@@ -56,14 +58,15 @@
 # -0.56453402316570156, -0.89329857536073409, -0.81062686366326098,
 # -0.61593843763923251, -0.67321374079183682, -0.82632908490446888]
 #
-#
 # Revision history:
 # 1. Minimal verion posted by Frans Slothouber on the forum.
 #    see link https://www.kaggle.com/c/two-sigma-financial-modeling/discussion/26044#148202
-# 2. Modification by Devin that add score function and bug fixes 
-#    See parent of this fork
+# 2. Modification by Devin that add score
+#    Parent of this fork
 # 3. Bug fix for the scoring function, and documentation
 #    This fork
+# 4. Bug fix for computation of final score. Thanks to PeterBruhn and vgoklani
+# 5. Fix in reset function, thanks to Devin
 #
 
 import pandas as pd
@@ -72,7 +75,8 @@ from sklearn.metrics import r2_score
 
 
 def r_score(y_true, y_pred, sample_weight=None, multioutput=None):
-    r2 = r2_score(y_true, y_pred, sample_weight=sample_weight, multioutput=multioutput)
+    r2 = r2_score(y_true, y_pred, sample_weight=sample_weight,
+                  multioutput=multioutput)
     r = (np.sign(r2)*np.sqrt(np.abs(r2)))
     if r <= -1:
         return -1
@@ -89,7 +93,7 @@ class Observation(object):
 
 class Environment(object):
     def __init__(self):
-        with pd.HDFStore("train.h5", "r") as hfdata:
+        with pd.HDFStore("../input/train.h5", "r") as hfdata:
             self.timestamp = 0
             fullset = hfdata.get("train")
             self.unique_timestamp = fullset["timestamp"].unique()
@@ -104,13 +108,15 @@ class Environment(object):
             self.train = fullset[fullset.timestamp < timesplit]
             self.test = fullset[fullset.timestamp >= timesplit]
 
-            # Just in case the full labels are needed later
-            self.y_test_full = self.test['y'] 
+            # Needed to compute final score
+            self.full = self.test.loc[:, ['timestamp', 'y']]
+            self.full['y_hat'] = 0.0
             self.temp_test_y = None
-            self.total_reward = 0
 
     def reset(self):
         timesplit = self.unique_timestamp[self.unique_idx]
+
+        self.unique_idx = int(self.n / 2)
         self.unique_idx += 1
         subset = self.test[self.test.timestamp == timesplit]
 
@@ -120,7 +126,7 @@ class Environment(object):
 
         target.loc[:, 'y'] = 0.0  # set the prediction column to zero
 
-        # changed bounds to 0:110 from 1:111 to mimic the behavior 
+        # changed bounds to 0:110 from 1:111 to mimic the behavior
         # of api for feature
         features = subset.iloc[:, :110].reset_index(drop=True)
 
@@ -128,19 +134,22 @@ class Environment(object):
         return observation
 
     def step(self, target):
+        timesplit = self.unique_timestamp[self.unique_idx-1]
+        # Since full and target have a different index we need
+        # to do a _values trick here to get the assignment working
+        y_hat = target.loc[:, ['y']]
+        self.full.loc[self.full.timestamp == timesplit, ['y_hat']] = y_hat._values
+
         if self.unique_idx == self.n:
             done = True
             observation = None
-            reward = self.total_reward/self.y_test_full.shape[0]
-            info = {'public_score': reward}
+            reward = r_score(self.temp_test_y, target.loc[:, 'y'])
+            score = r_score(self.full['y'], self.full['y_hat'])
+            info = {'public_score': score}
         else:
             reward = r_score(self.temp_test_y, target.loc[:, 'y'])
-            # keep track of total reward
-            self.total_reward += reward
-
             done = False
             info = {}
-
             timesplit = self.unique_timestamp[self.unique_idx]
             self.unique_idx += 1
             subset = self.test[self.test.timestamp == timesplit]
